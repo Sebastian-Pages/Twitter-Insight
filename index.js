4d52bdd57ce2e143b2bd4924b2f24c2532d2146c
Sebastian_Pages <yves-sebastian.pages@etu.u-bordeaux.fr>
2022-01-25 15:57:48 +0100

    forgot to push index

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const config = require('./config');
-const hbase = require('hbase');
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const hbase = require("hbase");
+
+const PORT = 7000;
+const client = hbase({ host: "127.0.0.1", port: 8080 });
 
 const app = express();
-const port = config.api_port;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(cors());
 
-app.use('/api/test', require('./routes/test'));
 
-app.listen(port, () => {
-  console.log(`server listening on ${port}`);
-})
+app.get("/t1", (req, res) => {
+    let result;
+    client.table("gresse_word_pop").scan(
+      {
+        batch: 100,
+      },
+      function (err, rows) {
+        result = {};
+        if (err) {
+          console.error(err);
+          res.status(400).send(err);
+          return;
+        }
+        result["words"] = [];
+        rows.forEach((element) => {
+          if (!result[element.key]) {
+            result[element.key] = [];
+          }
+          if (!result["words"].includes(element.key)) {
+            result["words"].push(element.key);
+          }
+          const obj = {};
+          obj.date = element.column.split(":")[1];
+          obj.total = element.$;
+          result[element.key].push(obj);
+        });
+        console.log(result);
+        res.status(200).send(result);
+        if (err) {
+          res.sendStatus(500);
+        }
+      }
+    );
+  });
+  
+
+app.get("/", (req, res) => res.status(400).send("Welcome 👌!"));
+
+app.listen(PORT, () => console.log("Server Listening"));
