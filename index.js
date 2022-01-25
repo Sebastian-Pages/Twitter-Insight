1d921394c32f50fa7e5e797565b52660bbda3b35
Sebastian_Pages <yves-sebastian.pages@etu.u-bordeaux.fr>
2022-01-25 15:52:01 +0100

    ok

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-a
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const hbase = require('hbase');
+
+const app = express();
+const port = config.api_port;
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use('/api/test', require('./routes/test'));
+
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
+})
