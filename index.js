fef2b1d3da6945c2c9edb7cd112451ddc55eab1c
Sebastian_Pages <yves-sebastian.pages@etu.u-bordeaux.fr>
2022-01-25 16:45:16 +0100

    server works for /

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const hbase = require("hbase");
+const morgan = require('morgan');
 
 const PORT = 7000;
+const hostname = '127.0.0.1';
 const client = hbase({ host: "127.0.0.1", port: 8080 });
 
 const app = express();
@@ -12,44 +14,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('combined'));
 
 
-app.get("/t1", (req, res) => {
-    let result;
-    client.table("gresse_word_pop").scan(
-      {
-        batch: 100,
-      },
-      function (err, rows) {
-        result = {};
-        if (err) {
-          console.error(err);
-          res.status(400).send(err);
-          return;
-        }
-        result["words"] = [];
-        rows.forEach((element) => {
-          if (!result[element.key]) {
-            result[element.key] = [];
-          }
-          if (!result["words"].includes(element.key)) {
-            result["words"].push(element.key);
-          }
-          const obj = {};
-          obj.date = element.column.split(":")[1];
-          obj.total = element.$;
-          result[element.key].push(obj);
-        });
-        console.log(result);
-        res.status(200).send(result);
-        if (err) {
-          res.sendStatus(500);
-        }
-      }
-    );
-  });
+app.get('/t1', (req,res) => {
+  client
+      .table('t1')
+      .row('0')
+      .get("total:value", (err,cell) => 
+          err ? res.sendStatus(404) : res.json(cell)
+      );
+});
   
 
-app.get("/", (req, res) => res.status(400).send("Welcome 👌!"));
+app.get("/", (req, res) => res.status(400).send("Welcome To Twitter Insight 🐦"));
 
-app.listen(PORT, () => console.log("Server Listening"));
+app.listen(PORT, hostname, () => console.log(`Server running at http://${hostname}:${PORT}/`));
