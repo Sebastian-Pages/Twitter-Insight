8e871d4c8855baa0ccd28100ac8a77142281f160
Sebastian_Pages <yves-sebastian.pages@etu.u-bordeaux.fr>
2022-01-28 21:06:29 +0100

    basic ejs page works with fake data

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Twitter-Insight
 A BigData project for university to monitor large amounts of tweets
+
+ssh -X ypages@147.210.117.54
+
+/usr/local/bin/reset_firefox.sh 
+
+curl -X GET -H "Accept: application/json" --negotiate -u: "http://lsd-prod-namenode-0.lsd.novalocal:8080/ypages:t1/0"
--- index.js
+++ index.js
@@ -1,69 +1,68 @@
-// const express = require("express");
-// const morgan = require("morgan");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const hbase = require("hbase");
+const express = require("express");
+const morgan = require("morgan");
 
-// const PORT = 7000;
-// const hostname = "127.0.0.1";
-// const client = hbase({ host: "http://lsd-prod-namenode-0.lsd.novalocal", port: 8080 });
-// const options = {
-//   hostname: 'http://lsd-prod-namenode-0.lsd.novalocal',
-//   port: 8080,
-//   path: '/ypages:t1/0',
-//   method: 'GET'
-// }
+const PORT = 7004;
+const hostname = "127.0.0.1";
 
-// const app = express();
-// app.use(morgan("combined"));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(cors());
+const app = express();
+app.use(morgan("combined"));
+app.set('view engine', 'ejs');
 
+app.get('/hashtags', (req, res) => {
+  var exec = require('child_process').exec;
 
-// app.get('/t1', (req, res) => {
-//   const scanner = client
-//       .table('ypages:t1')
-//       .scan({
-//           startRow: '0',
-//       })
+  var args = "-H 'Content-Type: application/json' https://next-js-project-manager.vercel.app/api/project";
 
-//   const rows = []
-
-//   scanner.on('readable', () => {
-//       let max = 0
-//       let chunk
-
-//       while ((chunk = scanner.read()) && max < req.params.batch) {
-//           rows.push(chunk)
-//           max++
-//       }
-//       scanner.emit('end', null);
-//   })
-
-//   scanner.on('error', err =>
-//       res.sendStatus(404)
-//   )
-
-//   scanner.on('end', () =>
-//       res.json(rows)
-//   )
-// });
-
-// app.get("/", (req, res) =>
-//   res.status(400).send("Welcome To Twitter Insight 🐦")
-// );
-
-// app.listen(PORT, hostname, () =>
-//   console.log(`Server running at http://${hostname}:${PORT}/`)
-// );
-
-const axios = require('axios');
-
-axios.get('http://lsd-prod-namenode-0.lsd.novalocal:8080/ypages:t1/0', { withCredentials: true })
-  .then(result => {
-    console.log(result.data);
-  })
-  .catch(error => {
-    console.log(error)
+  exec('curl ' + args, function (error, stdout, stderr) {
+    res.render('pages/hashtags',{ 
+      tagline: stdout,
+    });
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
   });
+});
+
+app.get('/users', (req, res) => {
+  var exec = require('child_process').exec;
+
+  var args = "-H 'Content-Type: application/json' https://next-js-project-manager.vercel.app/api/project";
+
+  exec('curl ' + args, function (error, stdout, stderr) {
+    res.render('pages/users',{ 
+      tagline: stdout,
+    });
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+});
+
+app.get('/influenceurs', (req, res) => {
+  var exec = require('child_process').exec;
+
+  var args = "-H 'Content-Type: application/json' https://next-js-project-manager.vercel.app/api/project";
+
+  exec('curl ' + args, function (error, stdout, stderr) {
+    res.render('pages/influenceurs',{ 
+      tagline: stdout,
+    });
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+});
+
+app.get("/", (req, res) =>
+  res.render('pages/index')
+);
+
+app.listen(PORT, hostname, () =>
+  console.log(`Server running at http://${hostname}:${PORT}/`)
+);
