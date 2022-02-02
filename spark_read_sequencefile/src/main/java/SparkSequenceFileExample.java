2d5a207b274a7b55835559360e3d74bc920628ba
Sebastian_Pages <yves-sebastian.pages@etu.u-bordeaux.fr>
2022-02-02 19:00:06 +0100

    saving count hashs to hbase

--- spark_read_sequencefile/src/main/java/SparkSequenceFileExample.java
+++ spark_read_sequencefile/src/main/java/SparkSequenceFileExample.java
@@ -1,6 +1,8 @@
 /**
  * @original author
  * Francieli Boito
+ * @modify author
+ * Sebastian Pages & Antoine Le Flohic
  */
 import java.io.IOException;
 
@@ -22,9 +24,26 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import sun.security.krb5.Config;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -44,17 +63,75 @@ import scala.Tuple2;
 
 
 public class SparkSequenceFileExample {
+	public static final byte[] ID_FAMILY = Bytes.toBytes("id");
+    public static final byte[] TEXT_FAMILY = Bytes.toBytes("text");
+    public static final String TABLE_NAME = "aleflohic:test"; //IL FAUT CHANGER LE NAMESPACE!
+    //get a line of the input file and transform it into a pair (city, population)
+	/*public static Tuple2<String,String> parseString(String s) {
+			String [] parsed = s.split(",");
+			Tuple2<String, String> ret = new Tuple2(parsed[1], parsed[4]);
+			return ret;
+	}*/
+
+	public static void createTable(Connection connect) {
+		try {
+			final Admin admin = connect.getAdmin();
+			//create a table with two column families: city and population
+			HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf(TABLE_NAME));
+			tableDescriptor.addFamily(new HColumnDescriptor(ID_FAMILY));
+			tableDescriptor.addFamily(new HColumnDescriptor(TEXT_FAMILY));
+			if (admin.tableExists(tableDescriptor.getTableName())) {
+				admin.disableTable(tableDescriptor.getTableName());
+				admin.deleteTable(tableDescriptor.getTableName());
+			}
+			admin.createTable(tableDescriptor);
+			admin.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
+	//get a tuple (city name, population) and prepare the corresponding tuple
+    //that will become a row in the hbase table
+    public static Tuple2<ImmutableBytesWritable, Put> prepareForHbase(Tuple2<Long,String> x) {
+        //the first element of the tuple, the city name, will be the key for the row
+        //that could be a problem for cities with the same name.
+        //However, this is just an example, we don't care.
+        //We could use zipWithIndex to add an index to each element of the RDD,
+        //that index could be used as a key (and would really be unique).
+        Put put = new Put(Bytes.toBytes(x._1()));
+        //we'll add two columns for this line, city:name (name is the name of the
+        //column, city is the family) and population:total
+        put.addColumn(ID_FAMILY, Bytes.toBytes("id"), Bytes.toBytes(x._1()));
+        put.addColumn(TEXT_FAMILY, Bytes.toBytes("text"), Bytes.toBytes(x._2()));
+        return new Tuple2<ImmutableBytesWritable, Put>(new ImmutableBytesWritable(), put);
+    }
+
 	public static void main(String[] args) throws Exception {
-	        //spark initialization part
+		//hbase initialization part
+        Configuration conf_hbase = HBaseConfiguration.create();
+		conf_hbase.set("hbase.mapred.outputtable", ID_TEXT);
+		conf_hbase.set("mapreduce.outputformat.class", "org.apache.hadoop.hbase.mapreduce.TableOutputFormat");
+		conf_hbase.set("mapreduce.output.fileoutputformat.outputdir", "/tmp");
+		Connection connection = ConnectionFactory.createConnection(conf_hbase);
+
+		//spark initialization part
 		SparkConf conf = new SparkConf().setAppName("Spark_SeqFile_example");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		sc.setLogLevel("ERROR"); //to see less messages from spark in the output
 		SparkSession spark = new SparkSession(JavaSparkContext.toSparkContext(sc)); //if we want to work with dataframes
+
 	    //read the input sequence file into an RDD
 	    JavaPairRDD<LongWritable,TweetWritable> inputfile = sc.sequenceFile("/user/fzanonboito/filtered_tweets/part*", LongWritable.class, TweetWritable.class); //here the types match what was used when writing the sequence file
         //we need to do two things: first we need to copy data from each element of the rdd produced by sequenceFile because it reuses the same Writable objects when reading
         //second we need to change the types because spark does not like the Hadoop Writable classes, it does not know how to serialize them.
         //here I just took the String field inside TweetWritable. Alternatively we could write our own Serializable class.
+		
+		// JavaPairRDD<LongWritable,TweetWritable> inputfile = sc.sequenceFile("/user/aleflohic/filtered_tweets/part*", LongWritable.class, TweetWritable.class); //here the types match what was used when writing the sequence file
+        // JavaRDD<Tuple2<Long, String>> parsed = inputfile.map(x -> new Tuple2<Long, String>(new Long(x._1.get()), x._2.text));
+        // JavaPairRDD<Long, String> inputmodified = JavaPairRDD.fromJavaRDD(parsed);
+
 		JavaRDD<Tuple2<String, Integer>> parsed = inputfile
 		    .filter(x -> !(x._2.hashtags.length==0))
 		    .flatMap(
@@ -77,5 +154,17 @@ public class SparkSequenceFileExample {
 		for (Tuple2<?,?> tuple : output){
 		    System.out.println(tuple._1()+" : "+tuple._2());
 		}
+
+		//create the hbase table where we'll write this
+        createTable(connection);
+
+		//prepare the data for hbase
+        JavaPairRDD<ImmutableBytesWritable, Put> hbaserdd = hbaseoutput.mapToPair(x -> prepareForHbase(x));
+       
+        //send it to hbase
+        Job newAPIJob = Job.getInstance(conf_hbase);
+        hbaserdd.saveAsNewAPIHadoopDataset(newAPIJob.getConfiguration());
+        System.out.println("saved to hbase\n");
+	}
 	}
 }
