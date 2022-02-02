38946bb2cb13816ccf6f60f28523056579c60a0e
Sebastian_Pages <yves-sebastian.pages@etu.u-bordeaux.fr>
2022-02-02 21:12:45 +0100

    api

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 
-const PORT = 7004;
+const PORT = 7005;
 const hostname = "127.0.0.1";
 
 const app = express();
 app.use(morgan("combined"));
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 app.get('/hashtags', (req, res) => {
   var exec = require('child_process').exec;
@@ -62,6 +62,25 @@ app.get('/influenceurs', (req, res) => {
 app.get("/", (req, res) =>
   res.render('pages/index')
 );
+
+//API
+
+app.get("/api/hashtagcount/:name", (req, res) =>
+  var exec = require('child_process').exec;
+
+  var args = " -X GET -H Accept: application/json --negotiate -u: \ http://lsd-prod-namenode-0.lsd.novalocal:8080/ypages:test3/"+req.params.name ;
+
+  exec('curl ' + args, function (error, stdout, stderr) {
+    res.json(stdout);
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+  
+);
+
 
 app.listen(PORT, hostname, () =>
   console.log(`Server running at http://${hostname}:${PORT}/`)
